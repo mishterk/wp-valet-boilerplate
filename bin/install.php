82d8a48df74f9cf2db8d613a3991e0250d7002d9
Phil Kurth <phil@philkurth.com.au>
2019-02-15 14:09:43 +1100

    changed install script

--- bin/install.php
+++ bin/install.php
@@ -1,22 +1,27 @@
 <?php
 
+/**
+ * This script will remove all work and the database. It will basically leave you with the boilerplate and the
+ * valetbp-config.php file.
+ */
+
 $config = require dirname( __DIR__ ) . '/valetbp-config.php';
 
 if ( $config['valet']['secure'] ) {
-	echo "Running 'valet secure'…";
+	echo "Running 'valet secure' (this will take a few seconds)" . PHP_EOL;
 	exec( 'valet secure' );
 }
 
-// install composer dependencies
+echo "Installing Composer dependencies" . PHP_EOL;
 exec( 'composer install --prefer-dist', $output );
 
-// create the DB
+echo "Creating the database" . PHP_EOL;
 $db_user = $config['db']['user'];
 $db_name = $config['db']['name'];
 $db_pass = $config['db']['password'] ? " -p $db_pass" : '';
 exec( "mysql -u {$db_user}{$db_pass} -e 'create database `{$db_name}`'" );
 
-// install wordpress
+echo "Downloading and installing WordPress" . PHP_EOL;
 $url   = $config['urls']['dev']['protocol'] . '//' . $config['urls']['dev']['host'];
 $title = $config['site']['title'];
 $user  = $config['auth']['username'];
@@ -25,10 +30,10 @@ $email = $config['auth']['email'];
 exec( 'wp core download' );
 exec( "wp core install --url='$url' --title='$title' --admin_user='$user' --admin_password='$pass' --admin_email='$email' --skip-email" );
 
-// install and activate the wp cli login helper plugin
+echo "Installing and activating the WP-CLI login helper plugin" . PHP_EOL;
 exec( 'wp login install --activate --yes' );
 
-// log me into the new site
-exec( 'wp valetbp log_me_in' );
+echo "Logging you into the site" . PHP_EOL;
+exec( 'wp valetbp login' );
 
-echo 'INSTALLATION COMPLETE';
+echo 'DONE: Installation complete' . PHP_EOL;

18fc5756bb0ba5a1692a51e8ab5b2af8c357e1cd
Phil Kurth <phil@philkurth.com.au>
2019-02-15 15:39:14 +1100

    better install script

--- bin/install.php
+++ bin/install.php
@@ -2,19 +2,29 @@
 
 $config = require dirname( __DIR__ ) . '/valetbp-config.php';
 
+$verbose        = false;
+$exec_and_print = function ( $command ) use ( $verbose ) {
+	exec( $command, $output );
+	if ( $verbose ) {
+		foreach ( $output as $line ) {
+			echo '  - ' . $line . PHP_EOL;
+		}
+	}
+};
+
 if ( $config['valet']['secure'] ) {
 	echo "Running 'valet secure' (this will take a few seconds)" . PHP_EOL;
-	exec( 'valet secure' );
+	$exec_and_print( 'valet secure' );
 }
 
 echo "Installing Composer dependencies" . PHP_EOL;
-exec( 'composer install --prefer-dist', $output );
+$exec_and_print( 'composer install --prefer-dist' );
 
 echo "Creating the database" . PHP_EOL;
 $db_user = $config['db']['user'];
 $db_name = $config['db']['name'];
 $db_pass = $config['db']['password'] ? " -p $db_pass" : '';
-exec( "mysql -u {$db_user}{$db_pass} -e 'create database `{$db_name}`'" );
+$exec_and_print( "mysql -u {$db_user}{$db_pass} -e 'create database `{$db_name}`'" );
 
 echo "Downloading and installing WordPress" . PHP_EOL;
 $url   = $config['urls']['dev']['protocol'] . '//' . $config['urls']['dev']['host'];
@@ -22,13 +32,19 @@ $title = $config['site']['title'];
 $user  = $config['auth']['username'];
 $pass  = $config['auth']['password'];
 $email = $config['auth']['email'];
-exec( 'wp core download' );
-exec( "wp core install --url='$url' --title='$title' --admin_user='$user' --admin_password='$pass' --admin_email='$email' --skip-email" );
+$exec_and_print( 'wp core download' );
+$exec_and_print( "wp core install --url='$url' --title='$title' --admin_user='$user' --admin_password='$pass' --admin_email='$email' --skip-email" );
+
+if ( $config['install']['plugins'] ) {
+	echo "Downloading and installing initial plugins" . PHP_EOL;
+	$plugins = implode( ' ', $config['install']['plugins'] );
+	$exec_and_print( "wp plugin install $plugins --activate" );
+}
 
 echo "Installing and activating the WP-CLI login helper plugin" . PHP_EOL;
-exec( 'wp login install --activate --yes' );
+$exec_and_print( 'wp login install --activate --yes' );
 
 echo "Logging you into the site" . PHP_EOL;
-exec( 'wp valetbp login' );
+$exec_and_print( 'wp valetbp login' );
 
 echo 'DONE: Installation complete' . PHP_EOL;

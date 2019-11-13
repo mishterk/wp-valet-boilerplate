d14539a4f7fe3d6ed81a0c5186a2a11e2ba6c3a9
Phil Kurth <phil@philkurth.com.au>
2019-11-14 07:40:11 +1100

    Add debug functions

--- lib/Debug.php
+++ lib/Debug.php
@@ -1,89 +1,134 @@
 <?php
 
-namespace WPValetBoilerplate;
 
-use DateTime;
-use DateTimeZone;
+namespace {
 
-class Debug {
 
-	public static $logfile;
+	use WPValetBoilerplate\Debug;
 
-	static function dd( $data ) {
-		echo '<pre>';
-		var_dump( $data );
-		echo '</pre>';
-		die();
+
+	if ( ! function_exists( 'dlog' ) ) {
+		function dlog( $data, $informative = false, $mode = 'a', $pretty = true ) {
+			Debug::log( $data, $informative, $mode, $pretty );
+		}
 	}
 
-	/**
-	 * Logs data to pds.log file
-	 *
-	 * @param mixed $data
-	 * @param string $mode specifies write mode ('a' for append || 'w' for write)
-	 * @param boolean $pretty if true, var_export is used
-	 * @param boolean $informative if true, informative header is printed
-	 *
-	 * @throws \Exception
-	 */
-	static function log( $data, $informative = false, $mode = 'w', $pretty = true ) {
-
-		if ( ! self::$logfile ) {
-			throw new \Exception( 'PDK\Debug::$logfile is not set' );
+	if ( ! function_exists( 'dd' ) ) {
+		function dd( $data ) {
+			ini_set( 'xdebug.var_display_max_depth', '10' );
+			ini_set( 'xdebug.var_display_max_children', '256' );
+			ini_set( 'xdebug.var_display_max_data', '1024' );
+			Debug::dd( $data );
 		}
-
-		$file_location = self::$logfile;
-
-		$datetime = new DateTime; // current time = server time
-		$otherTZ  = new DateTimeZone( 'Australia/Melbourne' );
-		$datetime->setTimezone( $otherTZ ); // calculates with new TZ now
-
-		$bt   = debug_backtrace();
-		$file = "Calling file: " . basename( $bt[0]['file'] );
-		$line = "Line: " . $bt[0]['line'];
-
-		$info = '';
-
-		if ( $informative ) {
-			$info = ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
-			$info .= $datetime->format( 'm/d/Y h:i:s a' ) . "\n";
-			$info .= $file . "\n";
-			$info .= $line . "\n";
-			$info .= ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
-		}
-
-		// Better boolean logging
-		if ( $data === false ) {
-			$data = '(bool)FALSE';
-		} elseif ( $data === true ) {
-			$data = '(bool)TRUE';
-		}
-
-		if ( $pretty ) {
-			$info .= print_r( $data, true );
-		} else {
-			$info .= var_export( $data, true );
-		}
-
-		$info .= $informative ? "\n\n" : "\n";
-
-		$file = fopen( $file_location, $mode ) or print( '<div style="background-color:#db514d;color:white;text-align:center;padding:10px;">Cannot open dev.log file for logging</div>' );
-		fwrite( $file, $info );
-		fclose( $file );
 	}
 
-	static function queryLogger( $query ) {
-		self::log( $query );
-
-		return $query;
+	if ( ! function_exists( 'qlog_start' ) ) {
+		function qlog_start() {
+			Debug::start_logging_queries();
+		}
 	}
 
-	static function startLoggingQueries() {
-		add_filter( 'query', [ __CLASS__, 'query_logger' ] );
+	if ( ! function_exists( 'qlog_end' ) ) {
+		function qlog_end() {
+			Debug::stop_logging_queries();
+		}
 	}
+}
 
-	static function stopLoggingQueries() {
-		remove_filter( 'query', [ __CLASS__, 'query_logger' ] );
+
+namespace WPValetBoilerplate {
+
+
+	use DateTime;
+	use DateTimeZone;
+
+
+	class Debug {
+
+		public static $logfile;
+
+
+		static function dd( $data ) {
+			echo '<pre>';
+			var_dump( $data );
+			echo '</pre>';
+			die();
+		}
+
+
+		/**
+		 * Logs data to pds.log file
+		 *
+		 * @param mixed $data
+		 * @param string $mode specifies write mode ('a' for append || 'w' for write)
+		 * @param boolean $pretty if true, var_export is used
+		 * @param boolean $informative if true, informative header is printed
+		 *
+		 * @throws \Exception
+		 */
+		static function log( $data, $informative = false, $mode = 'w', $pretty = true ) {
+
+			if ( ! self::$logfile ) {
+				throw new \Exception( 'PDK\Debug::$logfile is not set' );
+			}
+
+			$file_location = self::$logfile;
+
+			$datetime = new DateTime; // current time = server time
+			$otherTZ = new DateTimeZone( 'Australia/Melbourne' );
+			$datetime->setTimezone( $otherTZ ); // calculates with new TZ now
+
+			$bt = debug_backtrace();
+			$file = "Calling file: " . basename( $bt[0]['file'] );
+			$line = "Line: " . $bt[0]['line'];
+
+			$info = '';
+
+			if ( $informative ) {
+				$info = ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
+				$info .= $datetime->format( 'm/d/Y h:i:s a' ) . "\n";
+				$info .= $file . "\n";
+				$info .= $line . "\n";
+				$info .= ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n";
+			}
+
+			// Better boolean logging
+			if ( $data === false ) {
+				$data = '(bool)FALSE';
+			} elseif ( $data === true ) {
+				$data = '(bool)TRUE';
+			}
+
+			if ( $pretty ) {
+				$info .= print_r( $data, true );
+			} else {
+				$info .= var_export( $data, true );
+			}
+
+			$info .= $informative ? "\n\n" : "\n";
+
+			$file = fopen( $file_location, $mode ) or print( '<div style="background-color:#db514d;color:white;text-align:center;padding:10px;">Cannot open dev.log file for logging</div>' );
+			fwrite( $file, $info );
+			fclose( $file );
+		}
+
+
+		static function query_logger( $query ) {
+			self::log( $query, false, 'a' );
+
+			return $query;
+		}
+
+
+		static function start_logging_queries() {
+			add_filter( 'query', [ __CLASS__, 'query_logger' ] );
+		}
+
+
+		static function stop_logging_queries() {
+			remove_filter( 'query', [ __CLASS__, 'query_logger' ] );
+		}
+
 	}
 
 }
